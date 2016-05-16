938ed33306fc424403ce916657c63b65308286e1
Carlos Cardo <ccardo29@hotmail.com>
2016-05-16 18:56:47 -0400

    sin fetch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('payequipment'),
+    bucket: ENV('payequipment'),
     access_key_id: ENV['hotmail_usuario_name'],
     secret_access_key: ENV('hotmail_usuario_password'),    #.fetch
     s3_region: ENV.fetch('US Standard'),
