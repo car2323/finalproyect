1e3a6fd84a23f3be8400553f3280e6a7ddd21763
Carlos Cardo <ccardo29@hotmail.com>
2016-05-16 09:33:23 -0400

    fix image user  problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,12 @@ class ApplicationController < ActionController::Base
 
   before_action :permit_extra_divise_fields, if: :devise_controller?
   protect_from_forgery with: :exception
+
+
   protected
     def permit_extra_divise_fields
+      permit_array_edit = devise_parameter_sanitizer.for(:account_update)
+      permit_array_edit.push( :image)
     	permit_array = devise_parameter_sanitizer.for(:sign_up)
     	permit_array.push( :image, :name)
     end
