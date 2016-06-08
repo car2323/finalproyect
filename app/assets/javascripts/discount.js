bd9315a2afa65d8425428aa4b018818470f03b5a
Carlos Cardo <ccardo29@hotmail.com>
2016-06-08 19:12:20 -0400

    creating discount js function

---
+++ app/assets/javascripts/discount.js
@@ -0,0 +1,11 @@
+$(document).on("page:load ready", function () {
+	console.log("holaaaaaaa");
+     $("#js-discount").change(function(){
+
+     	$("#js-discount")
+     	
+          $(".js-pricelessrental")
+          $(".js-priceplusmainte")
+          $(".js-profit")
+     	});
+});
