b421d66954419c695fa23326f65f2939dd4abf80
Carlos Cardo <ccardo29@hotmail.com>
2016-06-08 21:31:15 -0400

    add to fixed 2

--- app/assets/javascripts/discount.js
+++ app/assets/javascripts/discount.js
@@ -6,17 +6,17 @@ $(document).on("page:load ready", function () {
      $("#js-discount").change(function(){
      	var porcent_discount = $("#js-discount").val();
      	var rental_discount = originalrental * porcent_discount;
-     	$(".js-pricelessrental").text( originalrental + rental_discount);
+     	$(".js-pricelessrental").text((originalrental + rental_discount).toFixed(2));
          console.log(rental_discount);
 /*--------------------------------------------------------------------------*/
 
         var rental_discount2 = originalpricemainte * porcent_discount;
-     	$(".js-priceplusmainte").text( originalpricemainte + rental_discount2);
+     	$(".js-priceplusmainte").text((originalpricemainte + rental_discount2).toFixed(2));
 
 /*--------------------------------------------------------------------------*/
         
         var rental_discount3 = originalprofit * porcent_discount;
-     	$(".js-profit").text( originalprofit - rental_discount3);
+     	$(".js-profit").text((originalprofit - rental_discount3).toFixed(2));
      	});
 });
 
