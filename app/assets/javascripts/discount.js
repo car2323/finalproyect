44efebb2f91bd3d1170f42202bdeaeccd63c14ea
Carlos Cardo <ccardo29@hotmail.com>
2016-06-08 21:15:19 -0400

    correction - for +

--- app/assets/javascripts/discount.js
+++ app/assets/javascripts/discount.js
@@ -1,5 +1,4 @@
 $(document).on("page:load ready", function () {
-	console.log("holaaaaaaa");
 	var originalrental = parseFloat($(".js-pricelessrental").text());
 	var originalpricemainte = parseFloat($(".js-priceplusmainte").text());
 	var originalprofit = parseFloat($(".js-profit").text());
@@ -7,19 +6,15 @@ $(document).on("page:load ready", function () {
      $("#js-discount").change(function(){
      	var porcent_discount = $("#js-discount").val();
      	var rental_discount = originalrental * porcent_discount;
-     	$(".js-pricelessrental").text( originalrental - rental_discount);
+     	$(".js-pricelessrental").text( originalrental + rental_discount);
          console.log(rental_discount);
 /*--------------------------------------------------------------------------*/
 
-
-
         var rental_discount2 = originalpricemainte * porcent_discount;
-     	$(".js-priceplusmainte").text( originalpricemainte - rental_discount2);
-
+     	$(".js-priceplusmainte").text( originalpricemainte + rental_discount2);
 
 /*--------------------------------------------------------------------------*/
         
-
         var rental_discount3 = originalprofit * porcent_discount;
      	$(".js-profit").text( originalprofit - rental_discount3);
      	});
