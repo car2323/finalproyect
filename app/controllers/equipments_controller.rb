378a03b56475fddd8896fa047d3a10da2997837f
Carlos Cardo <ccardo29@hotmail.com>
2016-11-11 10:20:48 -0500

    delete round 2

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -60,7 +60,7 @@ class EquipmentsController < ApplicationController
 #================================================================
     equipment.rentals.each do |one_rental|
           @total_rentals=@total_rentals+one_rental.total_price
-          @total_rentals.round(2)
+          @total_rentals.round
         end    
      end
 #================================================================
